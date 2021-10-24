816587be3d51cfdfb6f5a953a586e3906cf30cbd
reemsh2000 <sh951652@gmail.com>
2021-10-24 18:10:19 +0300

    signup validation at server side

---
+++ server/src/controllers/utilities/signupValidation.js.js
@@ -0,0 +1,11 @@
+const Joi = require('joi');
+
+const schema = Joi.object({
+  name: Joi.string().min(4).required(),
+  password: Joi.string().min(7).required(),
+  confirmPassword: Joi.ref('password'),
+  email: Joi.string().email().required(),
+  originalCity: Joi.string().required(),
+});
+
+module.exports = schema;
