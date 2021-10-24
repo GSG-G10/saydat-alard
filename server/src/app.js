145ff02b9cb061535c04625e0a95191a32051af7
Nizar7zak <nizar7zakout@gmail.com>
2021-10-24 12:54:15 +0300

    fix bug

---
+++ server/src/app.js
@@ -0,0 +1,28 @@
+require('env2')('.env');
+
+const express = require('express');
+const { join } = require('path');
+const compression = require('compression');
+const cookieParser = require('cookie-parser');
+
+const router =            require('../routes');
+
+const app = express();
+
+app.set('port', process.env.PORT || 5000);
+app.disable('x-powered-by');
+app.use(compression());
+app.use(cookieParser());
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+app.use(router);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(join(__dirname, '..', 'public')));
+  app.get('*', (req, res) => {
+    res.sendFile(join(__dirname, '..', 'client', 'build', 'index.html'));
+  });
+}
+
+module.exports = app;
