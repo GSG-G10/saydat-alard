aa1a25790a04b7a437451103d6f17b6bb98cad68
reemsh2000 <sh951652@gmail.com>
2021-10-24 18:07:50 +0300

    edit in queries

--- server/src/database/queries/addNewUser.js
+++ server/src/database/queries/addNewUser.js
@@ -1,4 +1,4 @@
 const connection = require('../config/connection');
 
-const addNewUser = (username, email, password, orginialTown) => connection.query('INSERT INTO users (username, email, password,orginialTown) VALUES($1,$2,$3,$4);', [username, email, password, orginialTown]);
+const addNewUser = (name, email, password, orginialTown) => connection.query('INSERT INTO users (name, email, password,orginialTown) VALUES($1,$2,$3,$4) RETURNING *;', [name, email, password, orginialTown]);
 module.exports = addNewUser;
