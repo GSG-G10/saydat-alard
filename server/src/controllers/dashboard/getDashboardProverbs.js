77486337877ad1a464d8e62a410448daf7dbb2bb
Nizar7zak <nizar7zakout@gmail.com>
2021-10-26 01:12:32 +0300

    handle pagination

--- server/src/controllers/dashboard/getDashboardProverbs.js
+++ server/src/controllers/dashboard/getDashboardProverbs.js
@@ -1,7 +1,18 @@
-// const { getDashboardProverbsQuery } = require('../../database/queries');
+const { getProverbQuery } = require('../../database/queries');
 
-// const getDashboardProverbs = async (req, res) => {
-//   const { page, char } = req.query;
-// };
+const getDashboardProverbs = async (req, res) => {
+  const { page, char } = req.query;
 
-// module.exports = getDashboardProverbs;
+  try {
+    const { rows, rowCount } = await getProverbQuery(char, page);
+    if (rowCount) {
+      res.status(200).json({ proverbs: rows });
+    } else {
+      res.status(404).json({ errorMsg: 'لا يوجد أمثال شعبية تبدأ بهذا الحرف' });
+    }
+  } catch (error) {
+    res.status(500).json({ errorMsg: 'حدث خطأ ما في السيرفر' });
+  }
+};
+
+module.exports = getDashboardProverbs;
--- server/src/database/queries/index.js
+++ server/src/database/queries/index.js
@@ -1,14 +1,13 @@
 const getMainCitiesQuery = require('./getMainCitiesQuery');
 const checkEmail = require('./checkEmail');
 const addNewUser = require('./addNewUser');
-const { approvePendingStory, editProverbQuery, getDashboardProverbsQuery } = require('./dashboard');
+const { approvePendingStory, editProverbQuery } = require('./dashboard');
 const getProverbQuery = require('./getProverbQuery');
 
 module.exports = {
   getMainCitiesQuery,
   checkEmail,
   addNewUser,
-  getDashboardProverbsQuery,
   approvePendingStory,
   editProverbQuery,
   getProverbQuery,
