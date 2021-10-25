8aa92e751a30c2e4b61a4fc635e2ed3bf6eb0b78
reemsh2000 <sh951652@gmail.com>
2021-10-25 23:14:30 +0300

    get dashboard stories

--- server/src/database/queries/index.js
+++ server/src/database/queries/index.js
@@ -1,9 +1,11 @@
 const getMainCitiesQuery = require('./getMainCitiesQuery');
 const checkEmail = require('./checkEmail');
 const addNewUser = require('./addNewUser');
+const { getStoriesQuery } = require('./dashboard');
 
 module.exports = {
   getMainCitiesQuery,
   checkEmail,
   addNewUser,
+  getStoriesQuery,
 };
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const {
-  checkUserExist, setCookie, getMainCities, signup, approvedStory, isAdmin, checkAuth,
+  checkUserExist, setCookie, getMainCities, signup, approvedStory, isAdmin,
+  checkAuth, getDashboardStories,
 } = require('../controllers');
 
 router.get('/', getMainCities);
-
+router.get('/dashboard/stories', getDashboardStories);
 router.post('/signup', checkUserExist, signup, setCookie);
 
 router.patch('/dashboard/story', checkAuth, isAdmin, approvedStory);
