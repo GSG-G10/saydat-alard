f0b4eca2d4999f4d9921d2a0f29f432b8d6becb1
Nizar7zak <nizar7zakout@gmail.com>
2021-10-25 17:34:44 +0300

    add auth middleware to route patch/dashboard

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 const {
-  checkUserExist, setCookie, getMainCities, signup, approvedStory,
+  checkUserExist, setCookie, getMainCities, signup, approvedStory, isAdmin, checkAuth,
 } = require('../controllers');
 
 router.get('/', getMainCities);
 
 router.post('/signup', checkUserExist, signup, setCookie);
 
-router.patch('/dashboard/story', approvedStory);
+router.patch('/dashboard/story', checkAuth, isAdmin, approvedStory);
 
 module.exports = router;
