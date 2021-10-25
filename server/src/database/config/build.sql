c4320376c4a093cd502937c10c4c46346c20b501
Nizar7zak <nizar7zakout@gmail.com>
2021-10-25 03:37:48 +0300

    insert data relates #71

--- server/src/database/config/build.sql
+++ server/src/database/config/build.sql
@@ -1,6 +1,10 @@
 BEGIN;
 
-DROP TABLE IF EXISTS users, stories, cities, families, proverbs CASCADE;
+DROP TABLE IF EXISTS users,
+stories,
+cities,
+families,
+proverbs CASCADE;
 
 CREATE TABLE users(
     id SERIAL PRIMARY KEY,
@@ -16,14 +20,9 @@ CREATE TABLE cities(
     name VARCHAR(50) NOT NULL,
     area VARCHAR(50),
     location VARCHAR(255),
-    occupationDate VARCHAR(255),
-    famousFor TEXT,
     image TEXT,
-    population VARCHAR(255),
-    populationDensity VARCHAR(255),
     quotation TEXT,
-    isMain BOOLEAN DEFAULT FALSE,
-    cityFrom VARCHAR(255)
+    isMain BOOLEAN DEFAULT FALSE
 );
 
 CREATE TABLE stories(
@@ -35,19 +34,12 @@ CREATE TABLE stories(
     city_id INTEGER REFERENCES cities(id) ON DELETE CASCADE
 );
 
-CREATE TABLE proverbs(
-    id SERIAL PRIMARY KEY,
-    content TEXT
-);
+CREATE TABLE proverbs(id SERIAL PRIMARY KEY, content TEXT);
 
 CREATE TABLE families(
     id SERIAL PRIMARY KEY,
     name TEXT,
     city_id INTEGER REFERENCES cities(id) ON DELETE CASCADE
 );
-
-
-INSERT INTO users(name, email, password, orginialTown, isAdmin) VALUES ('palestineAdmin', 'saydat.alard@gmail.com', '$2a$10$Bp9etHWYo1UAANX99Ju3LuEzTlH7d1mq8snUpUzV7UhfBlQwozIly', 'فلسطين', true);
-INSERT INTO users(name, email, password, orginialTown, isAdmin) VALUES ('Reem', 'reem@gmail.com', '$2a$10$Bp9etHWYo1UAANX99Ju3LuEzTlH7d1mq8snUpUzV7UhfBlQwozIly', 'حمامة', false);
 
 COMMIT;
