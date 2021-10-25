1cbb86724901034873318a59e9a125f69735c782
reemsh2000 <sh951652@gmail.com>
2021-10-26 00:24:23 +0300

    edit in query

--- server/src/controllers/main/getProverbs.js
+++ server/src/controllers/main/getProverbs.js
@@ -2,10 +2,10 @@ const { getProverbQuery } = require('../../database/queries');
 
 const getProverbs = async (req, res) => {
   const { char } = req.query;
+  console.log(char);
   try {
     const data = await getProverbQuery(char);
     const { rows } = data;
-    console.log(rows);
     res.status(200).json({ proverbs: rows });
   } catch (error) { res.status(500).json({ status: 500, msg: 'Server Error' }); }
 };
--- server/src/database/queries/getProverbQuery.js
+++ server/src/database/queries/getProverbQuery.js
@@ -1,11 +1,7 @@
 const connection = require('../config/connection');
 
-const aLetters = ['أ', 'إ', 'ا'];
 const getProverbQuery = (letter) => {
   const chars = `${letter}%`;
-  if (aLetters.includes(letter)) {
-    return connection.query("SELECT * from proverbs where content LIKE 'ا%' OR content LIKE 'إ%'OR content LIKE 'أ%'");
-  }
   return connection.query('SELECT * from proverbs where content LIKE $1', [chars]);
 };
 
