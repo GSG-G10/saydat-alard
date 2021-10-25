134db1cd5bc832b692e8b1c6ab796263db5f43b5
reemsh2000 <sh951652@gmail.com>
2021-10-25 23:38:01 +0300

    add pagination on backend for stories on dashboard

--- server/src/controllers/dashboard/getDashboardStories.js
+++ server/src/controllers/dashboard/getDashboardStories.js
@@ -1,11 +1,12 @@
+const { query } = require('../../database/config/connection');
 const { getStoriesQuery } = require('../../database/queries');
 
 const getDashboardStories = async (req, res) => {
+  const { page } = req.query;
   try {
-    const result = await getStoriesQuery();
+    const result = await getStoriesQuery(page);
     const { rows, rowCount } = result;
-    if (rowCount > 0) { res.status(200).json({ data: rows }); }
-    else res.status(200).json({ msg: 'لا يوجد قصص لعرضها ' });
+    if (rowCount > 0) { res.status(200).json({ data: rows }); } else res.status(200).json({ msg: 'لا يوجد قصص لعرضها ' });
   } catch (error) {
     res.status(500).json({ status: 500, msg: 'حدث خطأ ما في السيرفر' });
   }
--- server/src/database/queries/dashboard/getStoriesQuery.js
+++ server/src/database/queries/dashboard/getStoriesQuery.js
@@ -1,4 +1,4 @@
 const connection = require('../../config/connection');
 
-const getStoriesQuery = () => connection.query('select * from stories where approved=False');
+const getStoriesQuery = (page = 1) => connection.query('select * from stories where approved=False ORDER BY stories.id DESC LIMIT 5 OFFSET ($1 - 1) * 5', [page]);
 module.exports = getStoriesQuery;
