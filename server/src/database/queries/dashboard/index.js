d5a201006a7eea8ae0a15b47b622ebb2ad2be2a0
reemsh2000 <sh951652@gmail.com>
2021-10-26 00:52:46 +0300

    add proverb query

--- server/src/database/queries/dashboard/index.js
+++ server/src/database/queries/dashboard/index.js
@@ -1,4 +1,5 @@
 const approvePendingStory = require('./approverdStory');
 const editProverbQuery = require('./editProverbQuery');
+const addProverQuery = require('./addProverQuery');
 
-module.exports = { approvePendingStory, editProverbQuery };
+module.exports = { approvePendingStory, editProverbQuery, addProverQuery };
