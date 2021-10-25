c8c9e5a09972bcfdcb5a505b430c8e1e2bb97832
Nizar7zak <nizar7zakout@gmail.com>
2021-10-25 13:48:46 +0300

    patch/dashboard/story to approve story by admin relates #18

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
 
+const { approvedStory } = require('../controllers/dashboard');
+
 router.get('/', (req, res) => {
   res.json({ msg: 'hello' });
 });
+
+router.patch('/dashboard/story', approvedStory);
+
 module.exports = router;
