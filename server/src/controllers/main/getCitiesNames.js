088988a51be627127ca589294dc0a8593134756d
Nizar7zak <nizar7zakout@gmail.com>
2021-10-25 20:42:09 +0300

    handle error server

--- server/src/controllers/main/getCitiesNames.js
+++ server/src/controllers/main/getCitiesNames.js
@@ -4,9 +4,13 @@ const getCitiesNames = async (req, res) => {
   const { city } = req.query;
   try {
     const result = await getCitiesNamesQuery(city);
-    res.status(200).json({ data: result.rows });
+    if (result.rowCount) {
+      res.status(200).json({ data: result.rows });
+    } else {
+      res.status(404).json({ msg: 'لا يوجد مدينة تبدأ بهذا الحرف' });
+    }
   } catch (error) {
-    res.status(404).json({ msg: 'لا يوجد مدينة تبدأ بهذه الحروف' });
+    res.status(500).json({ msg: 'حدث خطأ ما في السيرفر' });
   }
 };
 
