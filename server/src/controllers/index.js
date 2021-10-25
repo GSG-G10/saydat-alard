311fdcd07008a6f7eb2d8acfcc0d4af06aac0b4d
Reem Shamia <71079908+reemsh2000@users.noreply.github.com>
2021-10-25 17:13:01 +0300

    Update index.js

--- server/src/controllers/index.js
+++ server/src/controllers/index.js
@@ -1,21 +1,15 @@
 const { getMainCities } = require('./main');
 const { signup } = require('./Auth');
-<<<<<<< HEAD
-const { checkUserExist, setCookie } = require('./middlewares');
-=======
 const {
   checkUserExist, setCookie, isAdmin, checkAuth,
 } = require('./middlewares');
->>>>>>> b9b0cfe86021c85a9ef54adb0aed9ed5d7b9bde2
+
 
 module.exports = {
   getMainCities,
   signup,
   checkUserExist,
   setCookie,
-<<<<<<< HEAD
-=======
   isAdmin,
   checkAuth,
->>>>>>> b9b0cfe86021c85a9ef54adb0aed9ed5d7b9bde2
 };
