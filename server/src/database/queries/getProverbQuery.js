efe42b4efc4bf361067a74618173709e2ca3d540
reemsh2000 <sh951652@gmail.com>
2021-10-25 19:12:09 +0300

    getProverbs query function

--- server/src/database/queries/getProverbQuery.js
+++ server/src/database/queries/getProverbQuery.js
@@ -2,9 +2,11 @@ const connection = require('../config/connection');
 
 const aLetters = ['أ', 'إ', 'ا'];
 const getProverbQuery = (letter) => {
+  const chars = `${letter}%`;
   if (aLetters.includes(letter)) {
     return connection.query("SELECT * from proverbs where content LIKE 'ا%' OR content LIKE 'إ%'OR content LIKE 'أ%'");
   }
-  return connection.query("SELECT * from proverbs where content LIKE '$1%'", [letter]);
+  return connection.query('SELECT * from proverbs where content LIKE $1', [chars]);
 };
+
 module.exports = getProverbQuery;
