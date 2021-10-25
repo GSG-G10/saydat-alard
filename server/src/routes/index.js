b643f21c403358838b74e1f6536a631413e86cd1
reemsh2000 <sh951652@gmail.com>
2021-10-26 00:10:54 +0300

    export/import for edit provrb function

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,12 +1,13 @@
 const router = require('express').Router();
 const {
-  checkUserExist, setCookie, getMainCities, signup, approvedStory, isAdmin, checkAuth,
+  checkUserExist, setCookie, getMainCities, signup, approvedStory, isAdmin,
+  checkAuth, editProvebDashboard,
 } = require('../controllers');
 
 router.get('/', getMainCities);
 
 router.post('/signup', checkUserExist, signup, setCookie);
-
+router.patch('/dashboard/proverb', checkAuth, isAdmin, editProvebDashboard);
 router.patch('/dashboard/story', checkAuth, isAdmin, approvedStory);
 
 module.exports = router;
