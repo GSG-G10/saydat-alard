1a8df9c5d3c976ac1fd4a1276d4d64935ff6c751
reemsh2000 <sh951652@gmail.com>
2021-10-26 16:20:15 +0300

    delete city dashboard function

---
+++ server/src/controllers/dashboard/deleteCityDashboard.js
@@ -0,0 +1,10 @@
+const { deleteCityDashboardQuery } = require('../../database/queries');
+
+const deleteCityDashboard = async (req, res) => {
+  const { cityId } = req.params;
+  try {
+    await deleteCityDashboardQuery(cityId);
+    res.json({ msg: 'تم التعديل بنجاح' });
+  } catch (error) { res.status(500).json({ status: 500, msg: 'Server Error' }); }
+};
+module.exports = deleteCityDashboard;
