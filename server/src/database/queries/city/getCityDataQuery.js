fdbcb8f7f14deb3396ac263668ba7c5de714beef
NadiaJalalTamimi <nadia.tamimi74@gmail.com>
2021-10-26 10:43:25 +0300

    make the get city data query

---
+++ server/src/database/queries/city/getCityDataQuery.js
@@ -0,0 +1,5 @@
+const connection = require('../../config/connection');
+
+const getCityDataQuery = (id) => connection.query('SELECT * FROM cities WHERE id=$1', [id]);
+
+module.exports = { getCityDataQuery };
