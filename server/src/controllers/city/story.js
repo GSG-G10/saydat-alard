9148a9d799dab80e600996969482093e81b6bfbc
mohammed Rayyes <mod.rayyes@gmail.com>
2021-10-27 08:26:56 +0300

    ..

--- server/src/controllers/city/story.js
+++ server/src/controllers/city/story.js
@@ -2,8 +2,7 @@ const { cloudinary } = require('../utilities/cloudinary');
 const { addStoryQuery } = require('../../database/queries/cityPage');
 
 const uploadStory = async (request, response) => {
-  const {content, title } = request.body;
-  // console.log();
+  const { content, title } = request.body;
 
   try {
     const fileStr = request.body.data;
@@ -12,7 +11,6 @@ const uploadStory = async (request, response) => {
     });
 
     addStoryQuery(content, title, uploadResponse.url);
-    console.log('bodyyyyyyy', request.body);
     response.json({ msg: 'uploaded successfully' });
   } catch (err) {
     response.status(500).json({ err: 'Something went wrong' });
