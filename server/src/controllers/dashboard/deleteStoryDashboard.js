7a7dec03e6ade129c9c62999318a1517a2b5f824
reemsh2000 <sh951652@gmail.com>
2021-10-27 14:35:41 +0300

    prevent delete story twice

--- server/src/controllers/dashboard/deleteStoryDashboard.js
+++ server/src/controllers/dashboard/deleteStoryDashboard.js
@@ -3,8 +3,10 @@ const { deleteStoryQuery } = require('../../database/queries');
 const deleteStoryDashboard = async (req, res) => {
   const { storyId } = req.params;
   try {
-    await deleteStoryQuery(storyId);
-    res.json({ msg: 'تم الحذف بنجاح' });
+    const result = await deleteStoryQuery(storyId);
+    if (result.rowCount > 0) {
+      res.json({ msg: 'تم الحذف بنجاح' });
+    } else res.status(400).json({ msg: 'Bad request' });
   } catch (error) { res.status(500).json({ status: 500, msg: 'Server Error' }); }
 };
 module.exports = deleteStoryDashboard;
