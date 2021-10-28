2ca3546f8aa187e538fd1f281617fde87a859e0e
NadiaJalalTamimi <nadia.tamimi74@gmail.com>
2021-10-28 21:23:35 +0300

    update file

--- server/src/controllers/city/cityData.js
+++ server/src/controllers/city/cityData.js
@@ -5,16 +5,16 @@ const getCityData = async (req, res, next) => {
   try {
     if (cityId > 0) {
       const { rows: cities } = await getCityDataQuery(cityId);
-      const { rows: stories } = await getstoryDataQuery(cityId);
-      if (!cities.length) {
+      if (cities.length) {
+        const { rows: stories } = await getstoryDataQuery(cityId);
         return res.json({
-          message: 'There is no city',
+          message: 'City Imported Successfuly',
+          cityData: cities[0],
+          stories,
         });
       }
       return res.json({
-        message: 'City Imported Successfuly',
-        cityData: cities[0],
-        stories,
+        message: 'There is no city',
       });
     }
     return res.json({ status: 400, message: 'Bad Request' });
