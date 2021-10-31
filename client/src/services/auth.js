1c25da6df2a173f7cab400b4a3c4aedb7901db98
reemsh2000 <sh951652@gmail.com>
2021-10-31 19:22:49 +0200

    checkAuth function

---
+++ client/src/services/auth.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+function ProtectedRoute(props) {
+  const { isAdmin, children } = props;
+
+  return (
+    isAdmin ? <Route><children /></Route> : <Redirect to={{ pathname: '/' }} />
+
+  );
+}
+
+export default ProtectedRoute;
