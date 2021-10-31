353df6a0a7721f1da94a30c0ab2e3364424661cd
reemsh2000 <sh951652@gmail.com>
2021-10-31 19:23:15 +0200

    add routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,50 @@
-import React from 'react';
+/* eslint-disable no-console */
+import React, { useEffect, useState } from 'react';
 import 'antd/dist/antd.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom';
+import ProtectedRoute from './services/auth';
+import Dashboard from './pages/Dashboard';
+import City from './pages/City';
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
+import Login from './pages/Login';
+import Error from './pages/Error';
+
+const axios = require('axios').default;
 
 function App() {
+  const [state, setstate] = useState(0);
+  // useEffect(() => () => {
+
+  // }, []);
+  axios.get('/userInfo').then(console.log).catch((error) => console.log(error));
   return (
-    <div className="App" />
+    <Router>
+      <Switch>
+        <Route path="/city/:id" component={City} />
+        <ProtectedRoute path="/dashboard" isAdmin={false}>
+          <Dashboard />
+        </ProtectedRoute>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/signup">
+          <SignUp />
+        </Route>
+        <Route path="/notfound">
+          <Error />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Redirect to="/notfound" />
+      </Switch>
+    </Router>
   );
 }
 
