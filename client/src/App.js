b672dc14daf0a36fdaa0a7d3cb0b90eb5e142d19
modrayyes <mod.rayyes@gmail.com>
2021-11-01 11:25:20 +0200

    config provider (rtl)

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import 'antd/dist/antd.less';
-import { Button } from 'antd';
 
 function App() {
   return (
-    <div className="App">
-      <Button type="primary"> ddsfsdf</Button>
-    </div>
+    <div className="App" />
   );
 }
 
