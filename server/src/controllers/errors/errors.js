520f5634097a64c772d57a7e057958b1a1c43d0a
linaYahya <linaebe0@gmail.com>
2021-11-01 09:11:04 +0200

    handle 500 err controller to handle differient status

--- server/src/controllers/errors/errors.js
+++ server/src/controllers/errors/errors.js
@@ -1,24 +1,18 @@
 /* eslint-disable no-unused-vars */
-
-const { httpResponse } = require('../../helpers');
-
-const errorCases = {
-  400: (res, message) => httpResponse.badRequest(res, message),
-  500: (res) => res.status(500).json({ message: 'internal server error' }),
+const error404 = (req, res, next) => {
+  res.status(404).json({ message: 'ما تبحث عنه غير موجود' });
 };
-
-const error404 = (req, res, next) => httpResponse.notFound(res, 'ما تبحث عنه غير موجود');
 
 const errors = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     // eslint-disable-next-line no-console
     console.log(err);
   }
-  const errorStatus = err.status || err.details ? 400 : 500;
-  if (errorCases[errorStatus]) {
-    return errorCases[errorStatus](res, err.message);
+  if (err.status) {
+    res.status(err.status).json({ message: err.message });
+  } else {
+    res.status(500).json({ message: 'internal server error' });
   }
-  return errorCases[500](res);
 };
 
 module.exports = { error404, errors };
