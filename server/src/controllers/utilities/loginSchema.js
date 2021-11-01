9fd31f5fac0f30732c053a29f0a4b96bfef6b69e
linaYahya <linaebe0@gmail.com>
2021-11-01 09:11:31 +0200

    validation changed to be less than 7 chars

--- server/src/controllers/utilities/loginSchema.js
+++ server/src/controllers/utilities/loginSchema.js
@@ -6,9 +6,7 @@ const loginValidation = Joi.object({
     'any.required': '    البريد الالكتروني مطلوب  ',
   }),
   password: Joi.string().required().min(7)
-    .regex(/^(?=\S*[a-z])(?=\S*[A-Z])(?=\S*\d)(?=\S*[^\w\s])\S{8,30}$/)
     .messages({
-      'string.pattern.base': '  يجب أن تحتوي كلمة المرور على الاقل على حرف واحد كبير و حرف واحد صغير ورمز و رقم  ',
       'string.empty': 'كلمة المرور لا يمكن أن تكون فارغة',
       'string.min': ' على اﻷقل{#limit}  كلمة المرور  يجب أن يحتوي على ',
       'any.required': '  كلمة المرور مطلوبة ',
