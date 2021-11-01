ad9d9ae05a423c81294f43cfa92c7ab65ec5381c
Nizar7zak <nizar7zakout@gmail.com>
2021-11-01 19:14:42 +0200

    paragraph text component

--- client/src/components/common/Paragraph.jsx
+++ client/src/components/common/Paragraph.jsx
@@ -4,12 +4,13 @@ import PropTypes from 'prop-types';
 
 const { Paragraph } = Typography;
 
-const ParagraphText = ({ text }) => (
-  <Paragraph>{text}</Paragraph>
+const ParagraphText = ({ text, strong }) => (
+  <Paragraph strong={strong}>{text}</Paragraph>
 );
 
 ParagraphText.propTypes = {
   text: PropTypes.string.isRequired,
+  strong: PropTypes.bool.isRequired,
 };
 
 export default ParagraphText;
