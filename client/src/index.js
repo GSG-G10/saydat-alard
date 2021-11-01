2abbe2f7e9178cbb58cb4ff14d7ecd4b9df0874e
NadiaJalalTamimi <nadia.tamimi74@gmail.com>
2021-11-01 12:08:22 +0200

    add the book component

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Footer from './components/common/Footer';
 import App from './App';
+import Bookcomponant from './components/bookcomponant/Bookcomponant';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
     <Footer />
+    <Bookcomponant />
   </React.StrictMode>,
   document.getElementById('root'),
 );
