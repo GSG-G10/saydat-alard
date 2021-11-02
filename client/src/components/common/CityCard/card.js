483dc5def4f50c5772719a6339f0b2826773a7e1
mohammed Rayyes <mod.rayyes@gmail.com>
2021-11-02 21:26:35 +0200

    slider style update

--- client/src/components/common/CityCard/card.js
+++ client/src/components/common/CityCard/card.js
@@ -5,10 +5,10 @@ import React from 'react';
 
 function Card({ title, children }) {
   return (
-      <div className="site-card-border-less-wrapper">
-        <CardComp title={title} bordered={false} headStyle={{textAlign: 'center', fontSize: '70px', borderWidth: 0}} style={{display: 'flex', flexDirection: 'column',  alignItems: 'center', width: 'auto', height: '100%'}} bodyStyle={{margin: 'auto'}} >
+      <div className="site-card-border-less-wrapper" style={{borderRadius: '50px'}}>
+        <CardComp title={title} bordered={false} headStyle={{textAlign: 'center', fontSize: '70px', borderWidth: 0, paddingTop:'100px'}} style={{display: 'flex', flexDirection: 'column',  alignItems: 'center', width: 'auto', height: '100%'}} bodyStyle={{margin: 'auto'}} >
             <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-                <div dir="rtl" style={{height: '100%'}}>
+                <div dir="rtl" style={{height: '50%',borderRadius: '50px'}}>
                     {children}
                 </div>
           </div>
