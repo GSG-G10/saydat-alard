6fe0d8f13395e34fd68edefad8fceb4c68b04406
NadiaJalalTamimi <nadia.tamimi74@gmail.com>
2021-11-02 11:04:36 +0200

    add a book page component

---
+++ client/src/components/bookcomponant/Page.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Page = React.forwardRef((props, ref) => (
+  <div className="demoPage" ref={ref}>
+    <h1>Page Header</h1>
+    <p>{props.children}</p>
+    <p>
+      Page number:
+      {props.number}
+    </p>
+  </div>
+));
+
+export default Page;
