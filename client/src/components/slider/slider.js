d89ccf86863590d5a4dec4c47b983a4f70add00e
mohammed Rayyes <mod.rayyes@gmail.com>
2021-11-02 18:46:00 +0200

    slider update

--- client/src/components/slider/slider.js
+++ client/src/components/slider/slider.js
@@ -1,36 +1,35 @@
-/* eslint-disable */
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-// import { useState, useEffect } from 'react';
 
 import { StackedCarousel } from 'react-stacked-carousel';
 import style from 'react-stacked-carousel/dist/index.css';
+import http from '../../services/httpService';
 import Card from '../common/CityCard/card';
 import './styles.css';
 
 function Slider() {
-  // eslint-disable-next-line no-unused-vars
   const [cities, setCities] = useState([]);
 
-  useEffect(async () => {
-    const getMainCities = () => {
-      const url = '/api/v1/';
-      return axios.get(url);
-    };
-
-    const response = await getMainCities().catch((err) => {
-      console.error(`Error in fetching cities ${err}`);
-    });
-
-    if (response) {
-      const { data } = response;
-      setCities(data);
-      console.log('data', data);
-    }
+  const getMainCities = () => {
+    const url = '/api/v1/';
+    return http
+      .get(url)
+      .then((response) => response)
+      .catch((err) => {
+        throw new Error(`Error in fetching products ${err}`);
+      });
+  };
+  useEffect(() => {
+    getMainCities()
+      .then((response) => {
+        if (response) {
+          const { data } = response;
+          setCities(data.data.cities);
+          console.log(data.data.cities);
+        }
+      });
   }, []);
 
   return (
-
     <div>
       <StackedCarousel
         autoRotate={false}
@@ -38,38 +37,32 @@ function Slider() {
         cardClassName={style.card}
         leftButton={<button type="button">Left</button>}
         rightButton={<button type="button">Right</button>}
-        style={{height: '733px', width: '727px', margin: 'auto', marginTop: '9%'}}
+        style={{
+          borderRadius: '50px',
+          height: '733px',
+          width: '727px',
+          margin: 'auto',
+          marginTop: '9%',
+        }}
       >
-        {[
-          {
-            id: 1, name: "حيفا", area: 20, population: 10000, density: 540 
-          },
-        ].map((city) => (
+        {cities.map((city) => (
           <Card key={city.id} title={city.name}>
-            <p>المساحة: {city.area} كيلومتر مربع</p>
-            <p>المساحة: {city.population} كيلومتر مربع</p>
-            <p>المساحة: {city.density} كيلومتر مربع</p>
+            <p>
+              المساحة:
+              {' '}
+              {city.area}
+              {' '}
+              كيلومتر مربع
+            </p>
+            <p>
+              الموقع:
+              {' '}
+              {city.location}
+              {' '}
+            </p>
           </Card>
         ))}
-        {/* <Card key="child2">
-          <img src="https://static-s.aa-cdn.net/img/ios/1567853765/e9f7f75687c9c8bbdab601421363ab5e?v=1" alt="" />
-
-        </Card>
-        <Card key="child3">
-          <img src="https://images.unsplash.com/photo-1542144612-1b3641ec3459?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="" />
-
-        </Card>
-        <Card key="child4">
-          <img src="https://images.unsplash.com/photo-1542261777448-23d2a287091c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" />
-
-        </Card>
-        <Card key="child5">
-          <img src="https://images.unsplash.com/photo-1542227844-5e56c7c2687d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="" />
-
-        </Card> */}
-
       </StackedCarousel>
-
     </div>
   );
 }
