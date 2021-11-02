aa0fb9ac2c2ee9e72c68f5c099ad85d9698afd93
reemsh2000 <sh951652@gmail.com>
2021-11-02 13:17:52 +0200

    edit in city information component

--- client/src/components/common/Img.jsx
+++ client/src/components/common/Img.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Img = ({
-  src, alt, styleClass, style,
+  src, alt, styleClass,
 }) => (
   <img
     loading="lazy"
     src={src}
     alt={alt}
     className={styleClass}
-    style={style}
   />
 );
 
@@ -17,6 +16,5 @@ Img.propTypes = {
   src: PropTypes.string.isRequired,
   alt: PropTypes.string.isRequired,
   styleClass: PropTypes.string.isRequired,
-  style: PropTypes.isRequired,
 };
 export default Img;
--- client/src/pages/City.jsx
+++ client/src/pages/City.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import CityInformation from '../components/CityInformation';
 
 function City() {
   return (
-    <div />
+    <CityInformation id={1} />
   );
 }
 
