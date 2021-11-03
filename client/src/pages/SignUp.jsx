fc22af4bf35c1e3b71a39349e746f40924e62931
reemsh2000 <sh951652@gmail.com>
2021-11-03 14:47:28 +0200

    signup

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import SignupForm from '../components/SignupForm';
 
 function SignUp() {
   return (
-    <div />
+    <div>
+      <SignupForm />
+    </div>
   );
 }
 
