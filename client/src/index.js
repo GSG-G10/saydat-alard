44de68e3a17eb9a43eda6af0dff4ace651d859fa
mohammed Rayyes <mod.rayyes@gmail.com>
2021-11-03 19:15:32 +0200

    adding story Form.

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { ConfigProvider } from 'antd';
 import App from './App/index';
-import StoryForm from './components/uploadStoryForm';
 
 ReactDOM.render(
   <ConfigProvider direction="rtl">
