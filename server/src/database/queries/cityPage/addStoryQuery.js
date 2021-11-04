a4ee2c0536f16e1882ca9eb9c265074983bda968
mohammed Rayyes <mod.rayyes@gmail.com>
2021-11-04 05:54:28 +0200

    returning data

--- server/src/database/queries/cityPage/addStoryQuery.js
+++ server/src/database/queries/cityPage/addStoryQuery.js
@@ -1,5 +1,5 @@
 const connection = require('../../config/connection');
 
-const addStoryQuery = (content, title, image, cityId, id) => connection.query('INSERT INTO stories (content, title, image, city_id, user_id) VALUES ($1, $2, $3, $4 ,$5)', [content, title, image, cityId, id]);
+const addStoryQuery = (content, title, image, cityId, id) => connection.query('INSERT INTO stories (content, title, image, city_id, user_id) VALUES ($1, $2, $3, $4 ,$5) returning *', [content, title, image, cityId, id]);
 
 module.exports = addStoryQuery;
