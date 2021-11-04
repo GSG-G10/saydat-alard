b84fd587ad9ada4e9ed4cea00271c1d1de7229c2
NadiaJalalTamimi <nadia.tamimi74@gmail.com>
2021-11-04 15:49:36 +0200

    update on the files

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { ConfigProvider } from 'antd';
 import App from './App/index';
-import Addproverbs from './components/Proverbs/Addproverbs';
+import Addproverbs from './components/Proverbs';
 
 ReactDOM.render(
   <ConfigProvider direction="rtl">
