87e8998aea57feef985f3b291d9af62f6efea2b2
reemsh2000 <sh951652@gmail.com>
2021-11-04 10:30:43 +0200

    change routes handle

--- client/src/App/index.js
+++ client/src/App/index.js
@@ -18,28 +18,52 @@ import Error from '../pages/Error';
 import AuthProvider from '../context/AuthContext';
 
 function App() {
+  const routes = [
+    {
+      path: '/city/:id',
+      children: <City />,
+      public: true,
+    },
+    {
+      path: '/dashboard',
+      children: <Dashboard />,
+      public: false,
+    },
+    {
+      path: '/login',
+      children: <Login />,
+      public: true,
+    },
+    {
+      path: '/signup',
+      children: <SignUp />,
+      public: true,
+    },
+    {
+      path: '/notfound',
+      children: <Error />,
+      public: true,
+    },
+    {
+      path: '/',
+      children: <Home />,
+      public: true,
+    },
+  ];
   return (
     <AuthProvider>
       <Router>
         <Switch>
-          <Route path="/city/:id">
-            <City />
-          </Route>
-          <ProtectedRoute path="/dashboard">
-            <Dashboard />
-          </ProtectedRoute>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/signup">
-            <SignUp />
-          </Route>
-          <Route path="/notfound">
-            <Error />
-          </Route>
-          <Route exact path="/">
-            <Home />
-          </Route>
+          {
+          routes.map((route) => (route.public
+            ? <Route exact path={route.path}>{route.children}</Route>
+            : (
+              <ProtectedRoute exact path={route.path}>
+                {route.children}
+
+              </ProtectedRoute>
+            )))
+        }
           <Redirect to="/notfound" />
         </Switch>
       </Router>
