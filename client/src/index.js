62a2df868766ba09e298250ebe634ac7a544d534
NadiaJalalTamimi <nadia.tamimi74@gmail.com>
2021-11-04 15:53:40 +0200

    remove proverb

--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { ConfigProvider } from 'antd';
 import App from './App/index';
-import Addproverbs from './components/Proverbs';
 
 ReactDOM.render(
   <ConfigProvider direction="rtl">
-    <React.StrictMode>
-      <App />
-      <Addproverbs />
-    </React.StrictMode>
+    <App />
   </ConfigProvider>,
   document.getElementById('root'),
 );
